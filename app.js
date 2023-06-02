ca96b7a2a7e4112f676f7da8faa4b001957d164a
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-02 00:55:43 -0400

    Creates simple colors

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const colors = ["green", "red", "#f15025", "rgba(133,122,200"];
+const colors = ["#00CC99", "#BC4749", "#1D4E89", "#F19A3E"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
     console.log(document.body);
+    //get random number betwee 0 - 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber]; color.textContent = colors[randomNumber]
 })
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
