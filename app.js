de01defba8b64dea8d73d7e018734f09a2195650
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-02 00:35:39 -0400

    Creates app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "#f15025", "rgba(133,122,200"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    console.log(document.body);
+})
